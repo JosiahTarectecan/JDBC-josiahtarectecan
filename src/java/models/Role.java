df266de40863774a0bb79c49253b5b799485c87a
Josiah Tarectecan <97994495+manemgeb@users.noreply.github.com>
2022-10-27 10:47:46 -0600

    Role class done

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -5,10 +5,54 @@
  */
 package models;
 
+import java.io.*;
+
 /**
  *
  * @author JosiahGabrielTarecte
  */
-public class Role {
+public class Role implements Serializable{
+       
+       private int roleId;
+       private String roleName;
+       
+       public Role(){
+              this.roleId = 0;
+              this.roleName = " ";
+       }
+              
+       public Role(int inputRoleID, String inputRoleName){
+              this.roleId = inputRoleID;
+              this.roleName = inputRoleName;
+       }
+       
+       public Role(int inputRoleID){
+              this.roleId = inputRoleID;
+              this.roleName = null;
+       }
+
+       public int getRoleId() {
+              return roleId;
+       }
+
+       public void setRoleId(int inputRoleID) {
+              this.roleId = inputRoleID;
+       }
+
+       public String getRoleName() {
+              return roleName;
+       }
+
+       public void setRoleName(String inputRoleName) {
+              this.roleName = inputRoleName;
+       }
+
+       @Override
+       public String toString() {
+              return "Role{" + "roleId=" + roleId + ", roleName=" + roleName + '}';
+       }
+ 
+       
+            
        
 }
